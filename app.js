133815ff66bea7369d5c37cbf129ba453da949ce
Yogendra Sankhla <yogendrasankhla@Yogendras-MacBook-Pro.local>
2023-01-21 15:33:15 +0530

    db initiate

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 const app = express();
 const PORT = 3000;
 
 // placeholder for the group messages
 let groupMessages = [];
 
+const db = new sqlite3.Database('groupmessages.db');
 
 
+// db.run(`CREATE TABLE group_messages (
+//     id INTEGER PRIMARY KEY,
+//     message TEXT,
+//     sender TEXT,
+//     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+// )`);
 
 // API to load all the group messages in a paginated manner
 app.get('/group-messages', (req, res) => {
